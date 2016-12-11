ddeff390ca18aa960f21d077ec7035fdd59a9a68
Enes Nehir Gürdamar <nehir.gurdamar@bilgiedu.net>
2016-12-11 15:51:53 +0300

    Filter html table

--- src/main/webapp/resources/core/js/main.js
+++ src/main/webapp/resources/core/js/main.js
@@ -48,48 +48,6 @@ function addAllColumnHeaders(myList,selector){
     return columnSet;
 }
 
-function searchDept() {
-    // Declare variables
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("searchDeparture");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("excelDataTable");
-    tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
-
-function searchDest() {
-    // Declare variables
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("searchDestination");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("excelDataTable");
-    tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[2];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
-
 function search() {
     // Declare variables
     var input, inputTwo, filter, filterTwo, table, tr, td, i;
