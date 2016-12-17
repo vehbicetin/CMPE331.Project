259e926f96d76405683a682553c4caa9a92ede47
Vehbi Çetin <vehbi.cetin@bilgiedu.net>
2016-12-17 15:24:40 +0200

    Controlling and rearranging codes, adding descriptor comments.

--- src/main/java/com/CMPE331/data/Bus.java
+++ src/main/java/com/CMPE331/data/Bus.java
@@ -1,10 +1,12 @@
+/*  CMPE331 Term Project.
+ *  Adem Aldemir, Enes Nehir Gürdamar, Emir Burak Selvi, Vehbi Çetin.
+ *  This class keeps the variables of the bus.
+ *  And making get and set functions of variables.
+ *  Created on 08.12.2016
+ */
+
 package com.CMPE331.data;
 
-import java.util.Date;
-
-/**
- * Created by Lenovo on 8.12.2016.
- */
 public class Bus {
 
     private int busID;
@@ -12,39 +14,48 @@ public class Bus {
     private String destination;
     private String time; // DD/MM/YYYY
 
-    public Bus(){}
+    public Bus() {
+    }
 
-    public Bus(int busID){
+    public Bus(int busID) {
         this.busID = busID;
     }
-    public Bus(int busID,String departure,String destination,String time){
-        this.busID=busID;
-        this.departure=departure;
-        this.destination=destination;
-        this.time=time;
+
+    public Bus(int busID, String departure, String destination, String time) {
+        this.busID = busID;
+        this.departure = departure;
+        this.destination = destination;
+        this.time = time;
     }
 
-    public int getBusID(){
+    public int getBusID() {
         return this.busID;
     }
-    public void setBusID(int busID){
+
+    public void setBusID(int busID) {
         this.busID = busID;
     }
+
     public String getDeparture() {
         return departure;
     }
+
     public void setDeparture(String departure) {
         this.departure = departure;
     }
+
     public String getDestination() {
         return destination;
     }
+
     public void setDestination(String destination) {
         this.destination = destination;
     }
+
     public String getTime() {
         return time;
     }
+
     public void setTime(String time) {
         this.time = time;
     }
