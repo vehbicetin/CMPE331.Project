8ae4685b64cbd3e0dc1edb6cc015f68c65e12b20
Vehbi Çetin <vehbi.cetin@bilgiedu.net>
2016-12-17 15:27:13 +0200

    Controlling and rearranging codes, adding descriptor comments.

--- src/main/java/com/CMPE331/web/controller/BusController.java
+++ src/main/java/com/CMPE331/web/controller/BusController.java
@@ -1,3 +1,10 @@
+/*  CMPE331 Term Project.
+ *  Adem Aldemir, Enes Nehir Gürdamar, Emir Burak Selvi, Vehbi Çetin.
+ *  This class making database autharization, controlling model and view,
+ *  and establishing connection between them.
+ *  Created on 05.12.2016
+ */
+
 package com.CMPE331.web.controller;
 
 import com.CMPE331.data.Bus;
@@ -12,10 +19,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Created by Lenovo on 5.12.2016.
- */
 
 @Controller
 public class BusController {
@@ -40,7 +43,7 @@ public class BusController {
 
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     @ResponseBody
-    public List<Bus> listBuses(){
+    public List<Bus> listBuses() {
 
         try {
 
@@ -50,27 +53,26 @@ public class BusController {
             Class.forName("com.mysql.jdbc.Driver");
 
             // Creating connection with the database
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/busautomation","cmpe","cmpe1234");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/busautomation", "cmpe", "cmpe1234");
 
             PreparedStatement ps = con.prepareStatement("SELECT * FROM busautomation.bus");
 
             // Execute select SQL statement
             ResultSet rs = ps.executeQuery();
 
-            while(rs.next()){
+            while (rs.next()) {
 
                 int id = rs.getInt(1);
                 String departure = rs.getString(2);
                 String destination = rs.getString(3);
                 String time = rs.getString(4);
-                Bus bus = new Bus(id,departure,destination,time);
+                Bus bus = new Bus(id, departure, destination, time);
                 busList.add(bus);
 
             }
 
             return busList;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
